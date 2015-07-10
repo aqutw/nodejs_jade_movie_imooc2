ec4a1fbcacd7839a518969675624da968c145db1
aqutw <aquarian.ex@gmail.com>
2015-07-10 23:16:45 +0800

    change routes url

--- config/routes.js
+++ config/routes.js
@@ -20,13 +20,13 @@ app.use(function(req, res, next){
     app.get('/signin', User.signin.get);
     app.post('/signin', User.signin.post);
     app.get('/logout', User.logout);
-    app.get('/admin/userlist', User.signinRequired, User.adminRequired, User.list);
+    app.get('/admin/user/list', User.signinRequired, User.adminRequired, User.list);
     
     app.get('/movie/:id', Movie.detail);
-    app.get('/admin/new', User.signinRequired, User.adminRequired, Movie['new']);
-    app.get('/admin/update/:id', User.signinRequired, User.adminRequired, Movie.update);
+    app.get('/admin/movie/new', User.signinRequired, User.adminRequired, Movie['new']);
+    app.get('/admin/movie/update/:id', User.signinRequired, User.adminRequired, Movie.update);
     app.post('/admin/movie/new', User.signinRequired, User.adminRequired, Movie.save);
-    app.get('/admin/list', User.signinRequired, User.adminRequired, Movie.list);
-    app.delete('/admin/list', User.signinRequired, User.adminRequired, Movie.del);
+    app.get('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.list);
+    app.delete('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.del);
 
 }//end module.exports

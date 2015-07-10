f7e66b09bed3b85df1bee3124596559ab4206397
aqutw <aquarian.ex@gmail.com>
2015-07-10 21:35:42 +0800

    Add session feature with MongoStore

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var path = require('path');
 var mongoose = require('mongoose');
 var _ = require('underscore');
@@ -13,7 +14,8 @@ var User = require('./models/user');
 var port = process.env.PORT || 3000;
 var app = express();
 
-mongoose.connect('mongodb://localhost/imooc')
+var DB_URL = 'mongodb://localhost/imooc';
+mongoose.connect(DB_URL);
 
 app.set('views', './views/pages' );
 app.set('view engine', 'jade');
@@ -21,7 +23,12 @@ app.set('view engine', 'jade');
 app.use(bodyParser()) // app.use(express.bodyParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser());
-app.use(session({secret: 'imooc'}));
+app.use(session({secret: 'imooc', 
+    store: new MongoStore({
+        url: DB_URL,
+        collection: 'sessions'
+    })
+}));
 
 app.locals.moment = require('moment');
 

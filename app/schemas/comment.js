2a55adf34b60d8f249f5a0103bf8ebc3b90a2fb0
aqutw <aquarian.ex@gmail.com>
2015-07-11 12:40:08 +0800

    define comment.js at schemas/comment.js

--- app/schemas/comment.js
+++ app/schemas/comment.js
@@ -1,27 +1,26 @@
 var mongoose = require('mongoose');
 
-var MovieSchema = new mongoose.Schema({
-   doctor: String,
-   title: String,
-   language: String,
-   country: String,
-   summary: String,
-   flash: String,
-   poster: String,
-   year: Number,
-   meta: {
-       createAt: {
-           type: Date,
-           default: Date.now()
-       },
-       updateAt: {
-           type: Date,
-           default: Date.now()
-       },
-   } 
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var CommentSchema = new mongoose.Schema({
+    movie: {type: ObjectId, ref: 'Movie'},
+    from: {type: ObjectId, ref: 'User'},
+    to: {type: ObjectId, ref: 'User'},
+    content: String,
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        },
+    } 
 });
 
-MovieSchema.pre('save', function(next){
+CommentSchema.pre('save', function(next){
     if (this.isNew) {
         this.meta.createAt = this.meta.updateAt = Date.now()
     } else {
@@ -31,7 +30,7 @@ MovieSchema.pre('save', function(next){
     next();
 });
 
-MovieSchema.statics = {
+CommentSchema.statics = {
     fetch: function(cb){
         return this.find({})
             .sort('meta.updateAt')
@@ -44,4 +43,4 @@ MovieSchema.statics = {
 }
 
 
-module.exports = MovieSchema;
+module.exports = CommentSchema;

8702d26621e3caa571bcd78ab5cbcf0d52b589fa
aqutw <aquarian.ex@gmail.com>
2015-07-11 21:51:38 +0800

    Add /results route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -25,3 +25,25 @@ exports.index = function(req, res){
     
     });*/
 };
+
+exports.search = function(req, res){
+    Category.find({})
+        .populate({path: 'movies', options: {limit: 5}})
+        .exec(function(err, categories){
+            if (err) {console.log(err);}
+            
+            res.render('index', {
+                title: 'imooc title',
+                categories: categories
+            });
+        });
+    
+    /*Movie.fetch(function(err, movies){
+        if (err){console.log(err);}
+    res.render('index', {
+        title: 'imooc index',
+        movies: movies
+    });
+    
+    });*/
+};
--- config/routes.js
+++ config/routes.js
@@ -36,5 +36,7 @@ app.use(function(req, res, next){
     app.get('/admin/category/new', User.signinRequired, User.adminRequired, Category['new']);
     app.post('/admin/category', User.signinRequired, User.adminRequired, Category.save);
     app.get('/admin/category/list', User.signinRequired, User.adminRequired, Category.list);
+    
+    app.get('/results', Index.search)
 
 }//end module.exports

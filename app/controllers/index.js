38bc7c5b53af7bb245475b87a14d2421a2149f6e
aqutw <aquarian.ex@gmail.com>
2015-07-11 14:41:31 +0800

    modify index via Category feature

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,27 @@
 var Movie = require('../models/movies');
+var Category = require('../models/category');
 
 var LOCALS_USER_KEY = 'user';
 var SESS_USER_KEY = 'user';
 
 exports.index = function(req, res){
-    console.log('req.session[SESS_USER_KEY]', req.session[SESS_USER_KEY]);
+    Category.find({})
+        .populate({path: 'movies', options: {limit: 5}})
+        .exec(function(err, categories){
+            if (err) {console.log(err);}
+            
+            res.render('index', {
+                title: 'imooc title',
+                categories: categories
+            });
+        });
     
-    Movie.fetch(function(err, movies){
+    /*Movie.fetch(function(err, movies){
         if (err){console.log(err);}
     res.render('index', {
         title: 'imooc index',
         movies: movies
     });
     
-    });
+    });*/
 };

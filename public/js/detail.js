3c9db80f7915b8a8340b6cb472a7af2b555f9fcd
aqutw <aquarian.ex@gmail.com>
2015-07-11 14:11:05 +0800

    more stable code logic

--- public/js/detail.js
+++ public/js/detail.js
@@ -3,17 +3,29 @@ $(function(){
         var $e=$(this);
         var toId = $e.data('tid');
         var commentId = $e.data('cid');
+        var $toId = $('#toId'),
+            $commentId = $('#commentId');
         
+        if( $toId.length ) {
+            $toId.val(toId);
+        } else { 
         $('<input>').attr({
             type: 'hidden',
+            id: 'toId',
             name: 'comment[tid]',
             value: toId
         }).appendTo('#commentForm');
+        }
         
+        if ( $commentId.length ) {
+            $commentId.val(commentId);
+        } else {
         $('<input>').attr({
             type: 'hidden',
+            id: 'commentId',
             name: 'comment[cid]',
             value: commentId
         }).appendTo('#commentForm');
+        }
     });
 });

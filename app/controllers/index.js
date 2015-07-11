4d3f1ee77648d41e575c7459d7cc1ea92891556f
aqutw <aquarian.ex@gmail.com>
2015-07-11 21:59:36 +0800

    Finish /results function page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -26,24 +26,23 @@ exports.index = function(req, res){
     });*/
 };
 
-exports.search = function(req, res){
-    Category.find({})
-        .populate({path: 'movies', options: {limit: 5}})
+
+exports.search = function(req, res){ //TODO: how to re-used index logic (Category.find)
+    var catId = req.query.cat;
+    var page = req.query.p;
+    var PAGESIZE = 2;
+    var index = page * PAGESIZE;
+    
+    Category.find({_id: catId})
+        .populate({path: 'movies', options: {limit: PAGESIZE, skip: index}})
         .exec(function(err, categories){
             if (err) {console.log(err);}
             
-            res.render('index', {
-                title: 'imooc title',
-                categories: categories
+            var category = categories[0] || {};
+            res.render('results', {
+                title: 'imooc Search Result page',
+                keyword: category.name,
+                category: category
             });
         });
-    
-    /*Movie.fetch(function(err, movies){
-        if (err){console.log(err);}
-    res.render('index', {
-        title: 'imooc index',
-        movies: movies
-    });
-    
-    });*/
 };
